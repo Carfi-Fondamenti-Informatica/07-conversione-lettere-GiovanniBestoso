75b3f43ef54ebafcb24c1a83beea3f003399ab01
GiovanniBestoso <115479602+GiovanniBestoso@users.noreply.github.com>
2022-12-05 14:54:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main(){
+   char a=0;
+    cin>>a;
+    if((funzione(a)== true)){
+        cout<<a;
+    }else if((funzione(a)==false)){
+        cout<<"errore"<<endl;
+    }
   return 0;
 }
